ea02d6849985a30269e278faafddf436919a6853
qin8948050 <275097304@qq.com>
2023-03-16 16:41:48 +0800

    perf(*): fill files

---
+++ cmd/server/wire.go
@@ -0,0 +1,23 @@
+//go:build wireinject
+// +build wireinject
+
+// The build tag makes sure the stub is not built in the final build.
+
+package main
+
+import (
+	"github.com/qin8948050/kratos-layout/internal/biz"
+	"github.com/qin8948050/kratos-layout/internal/conf"
+	"github.com/qin8948050/kratos-layout/internal/data"
+	"github.com/qin8948050/kratos-layout/internal/server"
+	"github.com/qin8948050/kratos-layout/internal/service"
+
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+)
+
+// wireApp init kratos application.
+func wireApp(*conf.Server, *conf.Data, log.Logger) (*kratos.App, func(), error) {
+	panic(wire.Build(server.ProviderSet, data.ProviderSet, biz.ProviderSet, service.ProviderSet, newApp))
+}
